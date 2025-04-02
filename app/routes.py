87d6d8973617d64257dc88fd3e4439db138ef336
sidghosh29 <sidghosh8953@gmail.com>
2025-04-02 17:02:30 +0530

    Page Navigation in Home and Profile Pages

--- app/routes.py
+++ app/routes.py
@@ -39,9 +39,30 @@ def index():
         is not a POST request anymore, and the refresh command works in a more predictable way.
         '''
     page = request.args.get('page', 1, type=int)
-    posts = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False).items
+    pagination_obj = db.paginate(current_user.following_posts(), page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    posts = pagination_obj.items
     
-    return render_template('index.html', title='Home', posts = posts, form = form)
+    next_url = url_for('index', page=pagination_obj.next_num) \
+        if pagination_obj.has_next else None
+    prev_url = url_for('index', page=pagination_obj.prev_num) \
+        if pagination_obj.has_prev else None
+    
+    return render_template('index.html', title='Home', posts = posts, form = form, next_url=next_url, prev_url=prev_url)
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    pagination_obj = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=pagination_obj.next_num) \
+        if pagination_obj.has_next else None
+    prev_url = url_for('explore', page=pagination_obj.prev_num) \
+        if pagination_obj.has_prev else None
+    posts = pagination_obj.items
+    return render_template("index.html", title='Explore', posts=posts, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -112,9 +133,19 @@ def register():
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
     # In the case that there are no results, the db.first_or_404 method automatically sends a 404 error back to the client.
-    posts = db.session.scalars(sa.select(Post).where(Post.user_id==user.id)).all()
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    pagination_obj = db.paginate(query, page=page,
+                        per_page=app.config['POSTS_PER_PAGE'],
+                        error_out=False)
+    posts = pagination_obj.items
+    next_url = url_for('user', username=user.username, page=pagination_obj.next_num) \
+        if pagination_obj.has_next else None
+    prev_url = url_for('user', username=user.username, page=pagination_obj.prev_num) \
+        if pagination_obj.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -176,12 +207,3 @@ def unfollow(username):
         return redirect(url_for('index'))
     
 
-@app.route('/explore')
-@login_required
-def explore():
-    
-    page = request.args.get('page', 1, type=int)
-    query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.paginate(query, page=page,
-                        per_page=app.config['POSTS_PER_PAGE'], error_out=False).items
-    return render_template("index.html", title='Explore', posts=posts)
