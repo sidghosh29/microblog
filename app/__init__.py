a4cc7f795cfb57231f23480b252d6ccddd5d7386
sidghosh29 <sidghosh8953@gmail.com>
2025-04-11 02:44:02 +0530

    Added errors blueprint and registed with main app.

--- app/__init__.py
+++ app/__init__.py
@@ -35,9 +35,10 @@ from flask_mail import Mail
 
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
+
 def get_locale():
-    # return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'hi'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return 'de'
 
 # Load environment variables from .env
 load_dotenv()
@@ -58,6 +59,10 @@ login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
+
+# register the blueprint
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -86,7 +91,9 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
 
-from app import routes, models, errors
+
+
+from app import routes, models
 '''
 The bottom import is a well known workaround that avoids circular imports, a common problem with Flask applications.
 '''
